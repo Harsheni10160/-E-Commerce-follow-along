c52c08975808751f644b9d74ae12b67b440701a7
harsheni10160 <devaharsheni2006@gmail.com>
2025-01-31 15:31:30 +0530

    done

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,7 +1,10 @@
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
+import Home from "./pages/Home";
+
 export{
     
     LoginPage,
      SignupPage,
+     Home
 };
